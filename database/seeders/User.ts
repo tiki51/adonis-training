180233326854fe969e1b3df7f42a613bb36f5a04
Steven Barber <stevenbarber@Stevens-MacBook-Pro.local>
2022-03-12 19:51:16 -0800

    init_commit

---
+++ database/seeders/User.ts
@@ -0,0 +1,20 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class UserSeeder extends BaseSeeder {
+  public static developmentOnly = true
+
+  public async run() {
+    await User.createMany([
+      {
+        email: 'virk@adonisjs.com',
+        password: 'secret',
+      },
+      {
+        email: 'romain@adonisjs.com',
+        password: 'supersecret',
+        rememberMeToken: 'false',
+      },
+    ])
+  }
+}
