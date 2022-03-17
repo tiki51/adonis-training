a53579e5c97a4d8f83035b43c4b9c369fde9f96c
yiqingtanggit <61526456+yiqingtanggit@users.noreply.github.com>
2022-03-16 22:28:18 -0700

    Added update method to post controller (#1)

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,3 +1,4 @@
+import { Response } from '@adonisjs/core/build/standalone'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
 
@@ -29,6 +30,20 @@ export default class PostsController {
     post.forumId = request.input('forum')
     await user.related('posts').save(post)
     return post
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    try {
+      const post = await Post.find(params.id)
+      if (post) {
+        post.title = request.input('title')
+        post.content = request.input('content')
+        post.forumId = request.input('forum')
+        await post.save()
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   public async destroy({
