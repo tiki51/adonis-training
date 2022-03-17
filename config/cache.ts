fef25de16bfaebed2e9773825e6fb05cde53e056
yiqingtanggit <yiqing.tang1@gmail.com>
2022-03-16 22:20:28 -0700

    Added update method to post controller

--- config/cache.ts
+++ config/cache.ts
@@ -11,46 +11,46 @@ import Env from '@ioc:Adonis/Core/Env'
 import { CacheConfig } from '@ioc:Kaperskyguru/Adonis-Cache'
 
 const cacheConfig: CacheConfig = {
-	driver: Env.get('CACHE_DRIVER') as string,
+  driver: Env.get('CACHE_DRIVER') as string,
 
-	drivers: {
-		file: {
-			driver: 'file',
-			path: 'cache/data',
-		},
+  drivers: {
+    file: {
+      driver: 'file',
+      path: 'cache/data',
+    },
 
-		array: {
-			driver: 'array',
-			serialize: false,
-		},
+    array: {
+      driver: 'array',
+      serialize: false,
+    },
 
-		database: {
-			driver: 'database',
-			table: 'cache',
-			connection: null,
-			lock_connection: null,
-		},
+    database: {
+      driver: 'database',
+      table: 'cache',
+      connection: null,
+      lock_connection: null,
+    },
 
-		redis: {
-			driver: 'redis',
-			connection: 'cache',
-			lock_connection: 'default',
-		},
+    redis: {
+      driver: 'redis',
+      connection: 'cache',
+      lock_connection: 'default',
+    },
 
-		memcached: {
-			driver: Env.get('CACHE_DRIVER', 'memcached') as string,
-			persistent_id: Env.get('MEMCACHED_PERSISTENT_ID') as string,
-			sasl: [Env.get('MEMCACHED_USERNAME') as string, Env.get('MEMCACHED_PASSWORD') as string],
-			options: {
-				// Memcached::OPT_CONNECT_TIMEOUT : 2000,
-			},
-			servers: {
-				host: Env.get('MEMCACHED_HOST', '127.0.0.1') as string,
-				port: Env.get('MEMCACHED_PORT', 11211) as string,
-				weight: 100,
-			},
-		},
-	},
+    memcached: {
+      driver: Env.get('CACHE_DRIVER', 'memcached') as string,
+      persistent_id: Env.get('MEMCACHED_PERSISTENT_ID') as string,
+      sasl: [Env.get('MEMCACHED_USERNAME') as string, Env.get('MEMCACHED_PASSWORD') as string],
+      options: {
+        // Memcached::OPT_CONNECT_TIMEOUT : 2000,
+      },
+      servers: {
+        host: Env.get('MEMCACHED_HOST', '127.0.0.1') as string,
+        port: Env.get('MEMCACHED_PORT', 11211) as string,
+        weight: 100,
+      },
+    },
+  },
 }
 
 export default cacheConfig
