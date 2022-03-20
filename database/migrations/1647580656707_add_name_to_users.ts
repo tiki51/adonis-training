72df93e45f2578633cc434f8ea1d6b90e1e0d0e3
yiqingtanggit <yiqing.tang1@gmail.com>
2022-03-19 19:23:07 -0700

    fixed formatting issues

--- database/migrations/1647580656707_add_name_to_users.ts
+++ database/migrations/1647580656707_add_name_to_users.ts
@@ -3,15 +3,15 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class AddNameToUsers extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.alterTable(this.tableName, (table) => {
       table.text('name').nullable()
     })
-    }
+  }
 
-  public async down () {
+  public async down() {
     this.schema.alterTable(this.tableName, (table) => {
       table.dropColumn('name')
     })
-    }
+  }
 }
--- database/migrations/1647738301306_add_image_link_to_posts.ts
+++ database/migrations/1647738301306_add_image_link_to_posts.ts
@@ -3,14 +3,15 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class AddImageLinkToPosts extends BaseSchema {
   protected tableName = 'posts'
 
-  public async up () {
+  public async up() {
     this.schema.alterTable(this.tableName, (table) => {
       table.text('image_link').nullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.dropColumn('image_link')  }
-    }
+      table.dropColumn('image_link')
+    })
   }
+}
